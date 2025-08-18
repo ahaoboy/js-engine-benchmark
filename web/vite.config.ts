21428476b8b1fecebb4b4391db45353029e591ee
ahaoboy <504595380@qq.com>
2025-08-18 14:50:46 +0800

    web: alias

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,29 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import analyzer from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "https://ahaoboy.github.io/js-engine-benchmark/",
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "antd",
-        "echarts",
-        "react-dom",
-        "react-dom/client",
-      ],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          antd: "antd",
-          echarts: "echarts",
-          "react-dom/client": "ReactDOM",
-        },
-      },
+  plugins: [react(),  analyzer({
+    "enabled": !!process.env["analyzer"],
+  })],
+  resolve: {
+    alias: {
+      react: "https://esm.sh/react",
+      "react-dom": "https://esm.sh/react-dom",
+      echarts: "https://esm.sh/echarts"
     },
-  },
+  }
 });
