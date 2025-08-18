c9945071e81eea9ccc60eff3ee5ca7088712e240
ahaoboy <504595380@qq.com>
2025-08-18 16:05:22 +0800

    web: alias

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,14 +5,17 @@ import analyzer from "vite-bundle-analyzer";
 // https://vite.dev/config/
 export default defineConfig({
   base: "https://ahaoboy.github.io/js-engine-benchmark/",
-  plugins: [react(),  analyzer({
-    "enabled": !!process.env["analyzer"],
-  })],
+  plugins: [
+    react(),
+    analyzer({
+      "enabled": !!process.env["analyzer"],
+    }),
+  ],
   resolve: {
     alias: {
       react: "https://esm.sh/react",
       "react-dom": "https://esm.sh/react-dom",
-      echarts: "https://esm.sh/echarts"
+      echarts: "https://esm.sh/echarts",
     },
-  }
+  },
 });
